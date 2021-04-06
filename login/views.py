5f6a12b1ecb3957c79bc65d84ab01cce248a645d
YunJaePark <54883589+YunJaePark3908@users.noreply.github.com>
2021-04-06 12:27:39 +0900

    User Id 중복값 제거 추가

--- login/views.py
+++ login/views.py
@@ -1,6 +1,7 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from .models import LoginUser
+from django.contrib.auth.hashers import make_password, check_password
 
 # Create your views here.
 
@@ -8,10 +9,20 @@ class RegistUser(APIView):
     def post(self, request):
         user_id = request.data.get('user_id')
         user_pw = request.data.get('user_pw')
+        user_pw_encrypted = make_password(user_pw)
+
+        #if user_id가 한글인지 영어인지 숫자인지 특수문자가 있는지 체크....
+
+        LoginUser.objects.filter(user_id=user_id).first()
+
+        if user_id is not None:
+            return Response(dict(msg="동일한 아이디가 있습니다."))
+
+        LoginUser.objects.create(user_id=user_id, user_pw=user_pw)
 
         data = dict(
             user_id=user_id,
-            user_pw=user_pw
+            user_pw=user_pw_encrypted
         )
 
         return Response(data)
